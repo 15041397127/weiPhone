21776d430f0eb9f37c8cda6051add462f99c1c7c
ZhangXu <zhangxu@zxdemacbook-pro.local>
2016-07-14 16:27:54 +0800

    Initial Commit

---
+++ weiPhone/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  weiPhone
+//
+//  Created by 张旭 on 16/7/14.
+//  Copyright © 2016年 zhangXu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ weiPhone/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  weiPhone
+//
+//  Created by 张旭 on 16/7/14.
+//  Copyright © 2016年 zhangXu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
